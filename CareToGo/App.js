eb24d716cb3d121fe4c45b0288cab7887217d7b3
ar1b <arib.kamal@ryerson.ca>
2022-07-01 08:57:47 -0400

    initialized new app

--- CareToGo/App.js
+++ CareToGo/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import HomeScreen from './src/screens/homescreen';
-import ResturantDetails from './src/screens/resturantdetails/RDindex';
-import DishDetails from './src/screens/dishdetails/dishdetails';
-import Basket from './src/screens/basket/basketdeets';
-import OrderScreen from './src/screens/orderscreen/orders';
-import OrderDetails from './src/screens/orderDetails/orderdetails';
-import {NavigationContainer} from '@react-navigation/native';
-import RootNavigator from './src/navigation/navigation';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <RootNavigator />
-        <StatusBar style='auto'/> 
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
