08b0a4118b1a8be369e97706a7ff6318ec507e1c
bylee777 <leebueyo@gmail.com>
2022-07-13 19:09:04 -0400

    test

--- CareToGo/src/components/appointmentComp/appointmentComp.js
+++ CareToGo/src/components/appointmentComp/appointmentComp.js
@@ -1,62 +1,76 @@
-import {View, StyleSheet, Text, TouchableOpacity, Pressable} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
 
 export default function AppointmentComp() {
+  const date = () => {
+    console.warn("The date should change dynamically");
+  };
 
-    const date = () => {
-        console.warn('The date should change dynamically')
-    }
+  const timeInfo = () => {
+    console.warn(
+      "The times of the appointment, name, and care profile should be dynamically rendered"
+    );
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <Pressable style={{ justifyContent: "center" }} onPress={date}>
+          <Text style={styles.titleContainer}>Monday, January 18th</Text>
+        </Pressable>
 
-    const timeInfo = () => {
-        console.warn('The times of the appointment, name, and care profile should be dynamically rendered')
-    }
-    return (
-        <View style={styles.container}>
-            <View style={styles.mainContainer}>
-                <Pressable style={{justifyContent: 'center'}} onPress={date}>
-                    <Text style = {styles.titleContainer}>Monday, January 19th</Text> 
-                </Pressable>
+        <TouchableOpacity
+          style={styles.appointmentContainer}
+          onPress={timeInfo}
+        >
+          <View>
+            <Text style={{ left: 10, fontWeight: "bold", marginVertical: 5 }}>
+              10 a.m.
+            </Text>
+            <Text style={{ left: 10, fontWeight: "bold" }}>11 a.m.</Text>
+          </View>
 
-                <TouchableOpacity style={styles.appointmentContainer} onPress={timeInfo}>
-                    <View>
-                        <Text style={{left: 10, fontWeight: 'bold', marginVertical: 5}}>10 a.m.</Text>
-                        <Text style={{left: 10, fontWeight: 'bold'}}>11 a.m.</Text> 
-                    </View>
-
-                    <View style={styles.infoContainer}>
-                        <Text style={{fontWeight: 'bold', marginVertical: 5}}>Yao Tian</Text>
-                        <Text>Standard Care Profile</Text> 
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+          <View style={styles.infoContainer}>
+            <Text style={{ fontWeight: "bold", marginVertical: 5 }}>
+              Yao Tian
+            </Text>
+            <Text>Standard Care Profile</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    },
+  container: {
+    flex: 1,
+  },
 
-    mainContainer: {
-        top: 20,
-        left: 25
-    },
+  mainContainer: {
+    top: 20,
+    left: 25,
+  },
 
-    titleContainer: {
-        fontWeight: 'bold',
-        fontSize: 20,
-        color: '#6C63FF',
-        marginBottom: 10
-    },
+  titleContainer: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#6C63FF",
+    marginBottom: 10,
+  },
 
-    appointmentContainer: {
-        backgroundColor: '#E2E0DF',
-        width: '80%',
-        paddingVertical: 20,
-        flexDirection: 'row'
-    },
-    infoContainer: {
-        marginLeft: 'auto',
-        right: 20
-    }
-})
+  appointmentContainer: {
+    backgroundColor: "#E2E0DF",
+    width: "80%",
+    paddingVertical: 20,
+    flexDirection: "row",
+  },
+  infoContainer: {
+    marginLeft: "auto",
+    right: 20,
+  },
+});
