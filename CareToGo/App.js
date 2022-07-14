8cf2ccad9e422e58e9c9c9da9189604539f21457
bylee777 <leebueyo@gmail.com>
2022-07-13 20:46:03 -0400

    amplify init

--- CareToGo/App.js
+++ CareToGo/App.js
@@ -1,45 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList, } from 'react-native';
-import StackNav from './src/navigation/stacknavigator/stacknav';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import StackNav from "./src/navigation/stacknavigator/stacknav";
+import { Amplify } from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native/dist/Auth";
 
+Amplify.configure(awsconfig);
 
-export default function App() {
-  return (
-    StackNav()
-  );
+function App() {
+  return StackNav();
 }
+
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   tabContainer: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   logContainer: {
-    position: 'absolute',
-    right: 50
+    position: "absolute",
+    right: 50,
   },
 
   buttonContainer: {
-    flexDirection: 'row',
-    position: 'absolute',
+    flexDirection: "row",
+    position: "absolute",
     top: 50,
-    left: 30
+    left: 30,
   },
 
   buttonBackground: {
-    backgroundColor: 'lightgray',
+    backgroundColor: "lightgray",
     width: 33,
     height: 33,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginHorizontal: 10,
-    borderRadius: 10
-  }
+    borderRadius: 10,
+  },
 });
