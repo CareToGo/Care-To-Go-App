db42ea191a2928dc806569bb1a58b84d9e552eb7
ar1b <arib.kamal@ryerson.ca>
2022-07-19 14:28:54 -0400

    completed edit profile and linked to profile about component

--- CareToGo/src/screens/profile/profileAbout.js
+++ CareToGo/src/screens/profile/profileAbout.js
@@ -1,93 +1,41 @@
 import React, {useState} from 'react';
 import {Text, View, ScrollView, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import DummyImage from '../../../assets/profile-dummy-image/emily.png'
-import HeaderButtons from './profile-header-buttons/HeaderButtons';
+import AboutComponent from './profile-screen-components/about/aboutComponent';
+import EditAbout from './profile-screen-components/about/editAbout';
 
 export default function ProfileAbout() {
 
     const [aboutEdit, setAboutEdit] = useState(false)
-    return(
-        <ScrollView>
-            <HeaderButtons/>
-            <View style={styles.mainContainer}>
-                <Image source={DummyImage} style={{left: 15, top: 15}}/>
-                <View style={styles.detailsContainer}>
-                    <Text style={{fontWeight: 'bold'}}>Emily Nelson</Text>
-                    <View style={styles.phone}>
-                        <Text style={{fontSize: 10, fontWeight: 'bold', color:'#B8C5D0'}}>Phone</Text>
-                        <Text>416-777-7777</Text>
-                    </View>
-                    <View style={styles.email}>
-                        <Text style={{fontSize: 10, fontWeight: 'bold', color:'#B8C5D0'}}>E-mail</Text>
-                        <Text>emily.n@hotmail.com</Text>
-                    </View>
-                    <View style={styles.date}>
-                        <Text style={{fontSize: 10, fontWeight: 'bold', color:'#B8C5D0'}}>Date of Birth</Text>
-                        <Text>December 07, 1981</Text>
-                    </View>                    
-                </View>
-            </View>
-            <View style={styles.bioContainer}>
-                <Text style={{fontSize: 20, left: 20, fontStyle: 'italic', top: 10}}>BIO</Text>
-                <Text style={{top: 10, fontSize: 12, paddingHorizontal: 15}}>
-                     ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..
-                </Text>
-            </View>
-            <View style={styles.emergencyContainer}>
-                <Text style={{paddingLeft: 20, paddingTop: 10, fontSize: 12, fontStyle: 'italic', fontWeight:'bold'}}>Emergency Contact</Text>
-                <View style={{paddingLeft: 70, paddingTop: 10}}>
-                    <Text>Tommy Sawyer</Text>
-                    <Text>647-420-420</Text>
-                </View>
-            </View>
 
-            <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Edit Profile</Text>
-            </TouchableOpacity> 
-        </ScrollView>
-    )
+    const setTrue = () => {
+        setAboutEdit(true)
+    }
+
+    const setFalse = () => {
+        setAboutEdit(false)
+    }
+
+    if(aboutEdit) {
+        return (
+            <View style={styles.container}>
+                <EditAbout setFalse={setFalse} />
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.container}>
+                <AboutComponent />
+                <TouchableOpacity style={styles.buttonContainer} onPress={setTrue}>
+                    <Text>Edit Profile</Text>
+                </TouchableOpacity> 
+            </View> 
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        backgroundColor: '#FFFFFF',
-        width: '80%',
-        height: '50%',
-        borderRadius: 20,
-        top: 30,
-        left: 45,
-        flexDirection: 'row'
-    },
-    detailsContainer: {
-        top: 30,
-        left: 25
-    },
-    phone: {
-        top: 10
-    },
-    email: {
-        top: 20
-    },
-    date: {
-        top: 30
-    },
-    bioContainer: {
-        backgroundColor: '#FFFFFF',
-        width: '80%',
-        height: '35%',
-        borderRadius: 20,
-        top: 50,
-        left: 45
-    },
-    
-    emergencyContainer: {
-        backgroundColor: '#FFFFFF',
-        width: '80%',
-        height: '20%',
-        borderRadius: 20,
-        top: 70,
-        left: 45,
-        flexDirection: 'row'
+    container: {
+        flex: 1
     },
     buttonContainer: {
         width: '40%',
@@ -98,9 +46,7 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         elevation: 3,
         backgroundColor: '#C4C4C4',
-        top: 90,
+        bottom: 70,
         left: 120
     },
-    
-    
 })
