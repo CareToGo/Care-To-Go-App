aa03776e682cecd4d159a69e3c4e19c56ab3906c
ar1b <arib.kamal@ryerson.ca>
2022-07-30 17:50:48 -0400

    Added email input

--- CareToGo/src/screens/paymentscreens/instantpayment.js
+++ CareToGo/src/screens/paymentscreens/instantpayment.js
@@ -1,31 +1,64 @@
 import { CardField, useStripe } from '@stripe/stripe-react-native';
-import { View, Text} from 'react-native'
+import { useState } from 'react';
+import { View, TextInput, StyleSheet, Text} from 'react-native';
 
 export default function InstantPayments() {
   const { confirmPayment } = useStripe();
+  const [ email, setEmail ] = useState(''); 
 
   return (
-    <CardField
-      postalCodeEnabled={true}
-      placeholders={{
-        number: '4242 4242 4242 4242',
-      }}
-      cardStyle={{
-        backgroundColor: '#FFFFFF',
-        textColor: '#000000',
-      }}
-      style={{
-        width: '100%',
-        height: 50,
-        marginVertical: 30,
-      }}
-      onCardChange={(cardDetails) => {
-        console.log('cardDetails', cardDetails);
-      }}
-      onFocus={(focusedField) => {
-        console.log('focusField', focusedField);
-      }}
-    />
+    <View style={styles.container}>
 
+        <View style={styles.paymentTitle}>
+            <Text style={{fontWeight: 'bold', fontSize: 30}}>A few more details...</Text>
+        </View>
+
+        <TextInput
+            placeholder='E-mail'
+            keyboardType='email-address'
+            style={styles.input}
+        />
+
+        <CardField
+            postalCodeEnabled={true}
+            placeholders={{
+                number: '4242 4242 4242 4242',
+            }}
+            cardStyle={styles.card}
+            style={styles.cardContainer}
+            onCardChange={(cardDetails) => {
+                console.log('cardDetails', cardDetails);
+            }}
+            onFocus={(focusedField) => {
+                console.log('focusField', focusedField);
+            }}
+        />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+    input: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 8,
+        fontSize: 20,
+        height: 50,
+        padding: 10,
+        marginHorizontal: 20
+    },
+
+    cardContainer: {
+        height: 50,
+        marginVertical: 30,
+        marginHorizontal: 20
+    },
+    card: {
+        backgroundColor: '#FFFFFF',
+
+    }
+})
