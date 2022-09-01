d293f6b92cc133a79ea301c4797a561bdb5629cd
leebueyo <67992712+leebueyo@users.noreply.github.com>
2022-08-31 21:59:24 -0400

    Update BasketContext.js

--- CareToGo/src/contexts/BasketContext.js
+++ CareToGo/src/contexts/BasketContext.js
@@ -1,16 +1,32 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { Auth, DataStore } from "aws-amplify";
 import { Basket } from "../models";
+import { useAuthContext } from "./AuthContext";
 
 const BasketContext = createContext({});
 
 const BasketContextProvider = ({ children }) => {
-  const addServiceToBasket = (Service) => {
-    console.log("added", Service);
-  };
+  const { dbUser } = useAuthContext();
+  const [basket, setBasket] = useState(null);
+  const [worker, setWorker] = useState(null);
+
+  // useEffect(() => {
+  //   DataStore.query(Basket, (b) =>
+  //     b.workerID("eq", worker.id).userID("eq", dbUser.id)
+  //   ).then((baskets) => setBasket(baskets[0]));
+  // }, [dbUser, worker]);
+
+  const addServiceToBasket = async (Service) => {};
+
+  // const createNewBasket = async () => {
+  //   const newBasket = await DataStore.save(
+  //     new Basket({ userID: dbUser.id, workerID: worker.id })
+  //   );
+  //   setBasket(newBasket);
+  // };
 
   return (
-    <BasketContext.Provider value={{ addServiceToBasket }}>
+    <BasketContext.Provider value={{ addServiceToBasket, setWorker }}>
       {children}
     </BasketContext.Provider>
   );
