b8f950c13b8a35f7c03907d9373988df920b933e
bylee777 <leebueyo@gmail.com>
2022-08-31 22:22:12 -0400

    payment

--- CareToGo/src/navigation/homestacknavigator/homestacknavigator.js
+++ CareToGo/src/navigation/homestacknavigator/homestacknavigator.js
@@ -1,26 +1,42 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Homescreen from "../../screens/homescreen/homescreen";
-import ContractorDetails from '../../screens/contractordetails/contractordetails';
-import OrderScreen from '../../screens/orderscreen/orderscreen';
-import Datepicker from '../../screens/datepicker/datepicker';
+import ContractorDetails from "../../screens/contractordetails/contractordetails";
+import OrderScreen from "../../screens/orderscreen/orderscreen";
+import Datepicker from "../../screens/datepicker/datepicker";
 import Appointments from "../../screens/appointments/appointments";
-import InstantPayments from "../../screens/paymentscreens/instantpayment";
-import ScheduledPayments from "../../screens/paymentscreens/scheduledpayment";
 
-const HomeStack = createNativeStackNavigator()
+const HomeStack = createNativeStackNavigator();
 
 const HomeStackNavigator = () => {
-    return (
-        <HomeStack.Navigator>
-            <HomeStack.Screen name='contrator-list' component={Homescreen} options={{headerShown: false}} />
-            <HomeStack.Screen name='contractor-details' component={ContractorDetails} options={{headerShown: false}}/>
-            <HomeStack.Screen name='orders' component={OrderScreen} options={{headerShown: false}}/>
-            <HomeStack.Screen name='date-picker' component={Datepicker} options={{headerShown: false}}/>
-            <HomeStack.Screen name='appointment-order' component={Appointments} options={{headerShown: false}}/>
-            <HomeStack.Screen name='instant-payment' component={InstantPayments} options={{headerShown: false}} />
-            <HomeStack.Screen name= 'scheduled-payments' component={ScheduledPayments} options={{headerShown: false}} />
-        </HomeStack.Navigator>
-    )
-}
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen
+        name="contrator-list"
+        component={Homescreen}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="contractor-details"
+        component={ContractorDetails}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="orders"
+        component={OrderScreen}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="date-picker"
+        component={Datepicker}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="appointment-order"
+        component={Appointments}
+        options={{ headerShown: false }}
+      />
+    </HomeStack.Navigator>
+  );
+};
 
 export default HomeStackNavigator;
--- CareToGo/src/screens/orderscreen/orderscreen.js
+++ CareToGo/src/screens/orderscreen/orderscreen.js
@@ -14,8 +14,8 @@ import { useNavigation } from "@react-navigation/native";
 import tw from "tailwind-react-native-classnames";
 import { useRef, useCallback, useState, useMemo } from "react";
 import { useBasketContext } from "../../contexts/BasketContext";
-import {CardForm, useStripe } from "@stripe/stripe-react-native";
-import BottomSheet, {BottomSheetView} from '@gorhom/bottom-sheet';
+import { CardForm, useStripe } from "@stripe/stripe-react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 const data = [
   {
@@ -85,8 +85,8 @@ const OrderScreen = () => {
 
   const [selected, setSelected] = useState(initialSelected);
 
-  const sheetRef = useRef(null)
-  const snapPoints = useMemo(() => ["50%"], []);
+  const sheetRef = useRef(null);
+  const snapPoints = useMemo(() => [370], []);
 
   const handleSheetChange = useCallback((index) => {
     console.log("handleSheetChange", index);
@@ -98,95 +98,117 @@ const OrderScreen = () => {
     sheetRef.current?.close();
   }, []);
 
-  const placeOrder = () =>  {
-
-  }
+  const placeOrder = () => {};
 
   return (
     <View style={styles.mainContainer}>
       <ScrollView nestedScrollEnabled={true} style={styles.container}>
-      <View>
-        <Text style={tw`text-center py-5 text-lg `}>Select</Text>
-      </View>
-      <View style={tw`border-t border-gray-200 flex`}></View>
+        <View>
+          <Text style={tw`text-center py-5 text-lg `}>Select</Text>
+        </View>
+        <View style={tw`border-t border-gray-200 flex`}></View>
 
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => (
-          <View style={tw` border-t border-gray-200 flex-shrink py-0`} />
-        )}
-        renderItem={({ item: { id, title, detail, price }, item }) => (
-          <TouchableOpacity
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          ItemSeparatorComponent={() => (
+            <View style={tw` border-t border-gray-200 flex-shrink py-0`} />
+          )}
+          renderItem={({ item: { id, title, detail, price }, item }) => (
+            <TouchableOpacity
+              onPress={() => {
+                let newSelected = { ...selected };
+                newSelected[item.id] = !newSelected[item.id];
+                setSelected(newSelected);
+                console.log(newSelected);
+              }}
+              style={tw`flex-row items-center justify-between p-5 ${
+                selected[id] && "bg-gray-200"
+              }`}
+            >
+              <View>
+                <Text style={tw`font-semibold text-lg`}>{title}</Text>
+                <Text style={tw`text-gray-500`}>{detail}</Text>
+              </View>
+              <Text style={tw`text-lg`}>${price}</Text>
+            </TouchableOpacity>
+          )}
+        />
+
+        <View style={styles.buttonContainer}>
+          <Pressable
+            style={styles.now}
             onPress={() => {
-              let newSelected = { ...selected };
-              newSelected[item.id] = !newSelected[item.id];
-              setSelected(newSelected);
-              console.log(newSelected);
+              handleSnapPress(0);
+              onAddToBasket();
             }}
-            style={tw`flex-row items-center justify-between p-5 ${
-              selected[id] && "bg-gray-200"
-            }`}
           >
-            <View>
-              <Text style={tw`font-semibold text-lg`}>{title}</Text>
-              <Text style={tw`text-gray-500`}>{detail}</Text>
-            </View>
-            <Text style={tw`text-lg`}>${price}</Text>
-          </TouchableOpacity>
-        )}
-      />
+            <Text style={{ color: "white" }}>Now</Text>
+          </Pressable>
 
-      <View style={styles.buttonContainer}>
-        <Pressable style={styles.now} onPress={() => handleSnapPress(0)}>
-          <Text style={{ color: "white" }}>Now</Text>
-        </Pressable>
-
-        <Pressable onPress={pressHandler} style={styles.later}>
-          <Text>Later</Text>
-        </Pressable>
-      </View>
-    </ScrollView>
-    <BottomSheet
+          <Pressable onPress={pressHandler} style={styles.later}>
+            <Text>Later</Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+      <BottomSheet
         ref={sheetRef}
         snapPoints={snapPoints}
         enablePanDownToClose={true}
       >
-      <BottomSheetView>
-        <View style={styles.name}>
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Your First Name"
+        <BottomSheetView>
+          <View style={styles.name}>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Your First Name"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Enter Your Last Name"
+            />
+          </View>
+          <CardForm
+            onFormComplete={(cardDetails) => {
+              console.log("card details", cardDetails);
+              setCard(cardDetails);
+            }}
+            style={{ height: 200, marginHorizontal: 10, top: 10 }}
           />
-          <TextInput
-            style={styles.input}
-            placeholder="Enter Your Last Name"
-          />
-        </View>
-        <CardForm
-          onFormComplete={(cardDetails) => {
-          console.log('card details', cardDetails);
-          setCard(cardDetails);
-        }}
-        style={{height: 200, marginHorizontal: 10, top: 10}}
-        />
-        <View style={styles.buttonContainer}>
-          <Pressable onPress={placeOrder} style={styles.placeOrder}>
-            <Text style={{fontWeight: 'bold', fontSize: 17, color:'#FFDE59', textAlign: 'center'}}>Place Order</Text>
-          </Pressable>
-          <Pressable onPress={() => handleClosePress()} style={styles.close}>
-            <Text style={{fontWeight: 'bold', fontSize: 17, color:'#FFFFFF', textAlign: 'center'}}>Close</Text>
-          </Pressable>
-        </View>
-      </BottomSheetView> 
-    </BottomSheet>
-    </View> 
+          <View style={styles.buttonContainer}>
+            <Pressable onPress={placeOrder} style={styles.placeOrder}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 17,
+                  color: "#FFDE59",
+                  textAlign: "center",
+                }}
+              >
+                Place Order
+              </Text>
+            </Pressable>
+            <Pressable onPress={() => handleClosePress()} style={styles.close}>
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 17,
+                  color: "#FFFFFF",
+                  textAlign: "center",
+                }}
+              >
+                Close
+              </Text>
+            </Pressable>
+          </View>
+        </BottomSheetView>
+      </BottomSheet>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   mainContainer: {
-    flex: 1
+    flex: 1,
   },
   container: {
     flex: 1,
@@ -237,34 +259,34 @@ const styles = StyleSheet.create({
     backgroundColor: "#C4C4C4",
   },
   name: {
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   input: {
     height: 40,
-    width: '44%',
+    width: "44%",
     marginHorizontal: 12,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     paddingHorizontal: 10,
-    marginTop: 20
+    marginTop: 20,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    marginLeft: 20
+    flexDirection: "row",
+    marginLeft: 20,
   },
   placeOrder: {
-    backgroundColor: '#001A72',
-    width: '55%',
-    borderRadius: 20,
-    paddingVertical: 20
-  },
-  close: {
-    backgroundColor: '#000000',
-    width: '35%',
+    backgroundColor: "#001A72",
+    width: "55%",
     borderRadius: 20,
     paddingVertical: 20,
-    marginLeft: 15
-  }
+  },
+  close: {
+    backgroundColor: "#000000",
+    width: "35%",
+    borderRadius: 20,
+    paddingVertical: 20,
+    marginLeft: 15,
+  },
 });
 
 export default OrderScreen;
