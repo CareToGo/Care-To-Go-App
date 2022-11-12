63f0ea2d31d5d7108774b9f63fbbe86e0315210c
Tony Fan <tony.bs.0303@gmail.com>
2022-11-11 20:23:06 -0500

    no more two apps

--- CareToWork/node_modules/@gorhom/bottom-sheet/lib/typescript/utilities/normalizeSnapPoint.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Converts a snap point to fixed numbers.
- */
-export declare const normalizeSnapPoint: (snapPoint: number | string, containerHeight: number, _topInset: number, _bottomInset: number, _$modal?: boolean) => number;
--- README.md
+++
@@ -1,1 +0,0 @@
-# Care-To-Go-App
